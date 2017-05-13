be8dadb4e166b2daade9348e7ed017523f6cb584
LilSaint <pamela.carric@gmail.com>
2017-05-13 13:17:06 +0200

    Gomme, trait, pinceau done

--- js/test.js
+++ js/test.js
@@ -1,8 +1,12 @@
-/*global console */
+/*global console, document, $ */
 
-$(function() {
+$(function () {
     'use strict';
 
+    /**
+     * Debug
+     * @param D
+     */
     function log(D) {
         console.log(D);
     }
@@ -13,59 +17,21 @@ $(function() {
         ctx,
         clickX          = [],
         clickY          = [],
+        startClickX     = [],
+        startClickY     = [],
         clickDrag       = [],
         paint,
         size,
         color,
         changeColor     = [],
         changeSize      = [];
-    log(canvas);
-    log(clickX);
-    log(clickY);
-
-    if (canvas.getContext) {
-        ctx = canvas.getContext('2d');
-
-        // On click avec la souris
-        canvas.addEventListener('mousedown', function (evt) {
-           var mouseX = evt.pageX - this.offsetLeft,
-               mouseY = evt.pageY - this.offsetTop;
-
-
-           paint = true;
-           canvasClick(evt.pageX - this.offsetLeft, evt.pageY - this.offsetTop);
-           drawCanvas();
-
-            // On bouge avec la souris
-            canvas.addEventListener('mousemove', function (evt) {
-                if (paint) {
-                    canvasClick(evt.pageX - this.offsetLeft, evt.pageY - this.offsetTop, true);
-                    drawCanvas();
-                }
-            });
-        });
-
-
-
-        // On relache le bouton de la souris
-        canvas.addEventListener('mouseup', function () {
-            paint = false;
-        });
-
-        // la souris quiite le canvas
-        canvas.addEventListener('mouseleave', function () {
-            paint = false;
-        });
-    }
 
     function drawCanvas() {
         var i;
 
-        // ctx.strokeStyle = '#000';
         ctx.lineJoin = 'round';
-        // ctx.lineWidth = 5;
 
-        for(i = 0; i < clickX.length; i += 1) {
+        for (i = 0; i < clickX.length; i += 1) {
             ctx.beginPath();
 
             if (clickDrag[i] && i) {
@@ -80,15 +46,116 @@ $(function() {
             ctx.stroke();
         }
     }
-    
+
+    /**
+     * dessine un trait
+     */
+    function trait() {
+        var i;
+
+        ctx.lineJoin = 'round';
+
+        for (i = 0; i < clickX.length; i += 1) {
+            ctx.beginPath();
+
+            // if (clickDrag[i] && i) {
+            //     ctx.moveTo(startClickX[i - 1], startClickY[i - 1]);
+            // } else {
+            //     ctx.moveTo(startClickX[i] - 1, startClickY[i]);
+            // }
+            ctx.moveTo(startClickX[i - 1], startClickY[i - 1]);
+            ctx.lineTo(clickX[i], clickY[i]);
+            ctx.closePath();
+            ctx.strokeStyle = changeColor[i];
+            ctx.lineWidth = changeSize[i];
+            ctx.stroke();
+        }
+
+    }
+
+    function rectangle() {
+        log('rectangle');
+    }
+
+    function rond() {
+        log('round');
+    }
+
+    /**
+     * Save the click position,
+     * options change {color, size, tools}
+     * @param x
+     * @param y
+     * @param dragging
+     */
     function canvasClick(x, y, dragging) {
         color = $('.colors').val();
         size  = $('.largeur').val();
-        
+
         clickX.push(x);
         clickY.push(y);
+
+        startClickX.push(x);
+        startClickY.push(y);
+
         clickDrag.push(dragging);
+
+        $('.gomme').click(function () {
+            changeColor.push('#FFF');
+        });
+
         changeColor.push(color);
         changeSize.push(size);
+    }
+
+    if (canvas.getContext) {
+        ctx = canvas.getContext('2d');
+
+        // On click avec la souris
+        canvas.addEventListener('mousedown', function (evt) {
+
+            paint = true;
+
+            canvasClick(evt.pageX - this.offsetLeft, evt.pageY - this.offsetTop);
+            drawCanvas();
+
+            // On bouge avec la souris
+            canvas.addEventListener('mousemove', function (evt) {
+                var self = this;
+                if (paint) {
+                    canvasClick(evt.pageX - self.offsetLeft, evt.pageY - self.offsetTop, true);
+                    drawCanvas();
+                }
+            });
+        });
+
+        $('.trait').click(function () {
+            canvas.addEventListener('mousedown', function (evt) {
+
+                paint = true;
+                canvasClick(evt.pageX - this.offsetLeft, evt.pageY - this.offsetTop);
+                trait();
+
+                // On bouge avec la souris
+                canvas.addEventListener('mousemove', function (evt) {
+                    var self = this;
+                    if (paint) {
+                        canvasClick(evt.pageX - self.offsetLeft, evt.pageY - self.offsetTop, true);
+                        trait();
+                    }
+                });
+            });
+
+        });
+        
+        // On relache le bouton de la souris
+        canvas.addEventListener('mouseup', function () {
+            paint = false;
+        });
+
+        // la souris quiite le canvas
+        canvas.addEventListener('mouseleave', function () {
+            paint = false;
+        });
     }
 });
