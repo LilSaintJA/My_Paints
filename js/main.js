895512b928b6db6ee50dd1c647735bb69c175af7
LilSaint <pamela.carric@gmail.com>
2017-05-14 20:21:25 +0200

    Ajout de la feature download

--- js/main.js
+++ js/main.js
@@ -20,27 +20,28 @@ $(function () {
         mouse           = {x: 0, y: 0},
         last_mouse      = {x: 0, y: 0},
         start_mouse     = {x: 0, y: 0},
-        ppts            = [];
+        ppts            = [],
+        tool            = 'pencil';
 
-    var tool = 'brush';
+
     $('#tools button').click(function () {
         tool = $(this).attr('id');
         log(tool);
     });
 
-    tmpCanvas.className = 'tmpCanvas';
-    tmpCanvas.width = canvas.width;
-    tmpCanvas.height = canvas.height;
-
-    paint.appendChild(tmpCanvas);
-
-    log(canvas);
+    if (tool === 'gomme') {
+        // Hide Tmp Canvas
+        tmpCanvas.style.display = 'none';
+    } else if (tool === 'pencil') {
+        tmpCanvas.style.display = 'block';
+    }
 
     /**
      * Dessine le canvas sans options
-     * 
+     *
      */
     function drawCanvas() {
+
         tmpCtx.beginPath();
         tmpCtx.moveTo(last_mouse.x, last_mouse.y);
         tmpCtx.lineTo(mouse.x, mouse.y);
@@ -85,14 +86,31 @@ $(function () {
      *
      */
     function rectangle() {
+        tmpCtx.clearRect(0, 0, tmpCanvas.width, tmpCanvas.height);
+
         var x       = Math.min(mouse.x, start_mouse.x),
             y       = Math.min(mouse.y, start_mouse.y),
             width   = Math.abs(mouse.x - start_mouse.x),
             height  = Math.abs(mouse.y - start_mouse.y);
 
         tmpCtx.globalAlpha = 1;
-        tmpCtx.clearRect(0, 0, tmpCanvas.width, tmpCanvas.height);
         tmpCtx.strokeRect(x, y, width, height);
+    }
+
+    /**
+     * Dessine un rectangle plein
+     *
+     */
+    function rectangleFull() {
+        tmpCtx.clearRect(0, 0, tmpCanvas.width, tmpCanvas.height);
+
+        var x       = Math.min(mouse.x, start_mouse.x),
+            y       = Math.min(mouse.y, start_mouse.y),
+            width   = Math.abs(mouse.x - start_mouse.x),
+            height  = Math.abs(mouse.y - start_mouse.y);
+
+        tmpCtx.globalAlpha = 1;
+        tmpCtx.fillRect(x, y, width, height);
     }
 
     /**
@@ -120,23 +138,67 @@ $(function () {
     }
 
     /**
+     * Dessine un cercle plein
+     *
+     */
+    function circleFull() {
+        tmpCtx.clearRect(0, 0, tmpCanvas.width, tmpCanvas.height);
+
+        var x,
+            y,
+            radius;
+        x = (mouse.x + start_mouse.x) / 2;
+        y = (mouse.y + start_mouse.y) / 2;
+
+        radius = Math.max(
+            Math.abs(mouse.x - start_mouse.x),
+            Math.abs(mouse.y - start_mouse.y)
+        ) / 2;
+
+        tmpCtx.globalAlpha = 1;
+        tmpCtx.beginPath();
+        tmpCtx.arc(x, y, radius, 0, Math.PI * 2, false);
+        tmpCtx.fill();
+        tmpCtx.closePath();
+    }
+
+    /**
      * Gomme
      *
      */
     function erase() {
+        // Saving all the points in an array
+        ppts.push({x: mouse.x, y: mouse.y});
 
         ctx.globalCompositeOperation = 'destination-out';
-        ctx.fillStyle = '#FFF';
-        ctx.strokeStyle = '#FFF';
+        ctx.fillStyle = 'rgba(255, 255, 255, 1)';
+
+        if (ppts.length < 3) {
+            ctx.beginPath();
+            ctx.fill();
+            ctx.closePath();
+
+            return false;
+        }
+
+        ctx.beginPath();
+        ctx.moveTo(ppts[0].x, ppts[0].y);
+
         ctx.stroke();
-        ctx.closePath();
     }
 
     /* ***************************************************************** */
     /* ************************ CANVAS BY DEFAULT ********************* */
     /* ***************************************************************** */
     if (canvas.getContext) {
+        log(tool);
         ctx = canvas.getContext('2d');
+
+        tmpCanvas.className = 'tmpCanvas';
+        tmpCanvas.width = canvas.width;
+        tmpCanvas.height = canvas.height;
+
+        paint.appendChild(tmpCanvas);
 
         tmpCtx.lineJoin = 'round';
         tmpCtx.lineCap = 'round';
@@ -151,6 +213,7 @@ $(function () {
         $('.colors').on('change', function () {
             var color = $('.colors').val();
             tmpCtx.strokeStyle = color;
+            tmpCtx.fillStyle = color;
         });
 
         tmpCanvas.addEventListener('mousemove', function (evt) {
@@ -178,7 +241,13 @@ $(function () {
             mouse.x = typeof evt.offsetX !== 'undefined' ? evt.offsetX : evt.layerX;
             mouse.y = typeof evt.offsetY !== 'undefined' ? evt.offsetY : evt.layerY;
         }, false);
-        
+
+        canvas.addEventListener('mousemove', function(evt) {
+
+            mouse.x = typeof evt.offsetX !== 'undefined' ? evt.offsetX : evt.layerX;
+            mouse.y = typeof evt.offsetY !== 'undefined' ? evt.offsetY : evt.layerY;
+        }, false);
+
         tmpCanvas.addEventListener('mousedown', function (evt) {
             mouse.x = typeof evt.offsetX !== 'undefined' ? evt.offsetX : evt.layerX;
             mouse.y = typeof evt.offsetY !== 'undefined' ? evt.offsetY : evt.layerY;
@@ -186,7 +255,10 @@ $(function () {
             start_mouse.x = mouse.x;
             start_mouse.y = mouse.y;
 
-            if (tool === 'brush') {
+            if (tool === 'pencil') {
+                tmpCanvas.addEventListener('mousemove', drawCanvas, false);
+                drawCanvas();
+            } else if (tool === 'brush') {
                 tmpCanvas.addEventListener('mousemove', brush, false);
                 brush();
             } else if (tool === 'trait') {
@@ -195,27 +267,45 @@ $(function () {
             } else if (tool === 'rectangle') {
                 tmpCanvas.addEventListener('mousemove', rectangle, false);
                 rectangle();
+            } else if (tool === 'rectangleFull') {
+                tmpCanvas.addEventListener('mousemove', rectangleFull, false);
+                rectangleFull();
             } else if (tool === 'circle') {
                 tmpCanvas.addEventListener('mousemove', circle, false);
                 circle();
+            } else if (tool === 'circleFull') {
+                tmpCanvas.addEventListener('mousemove', circleFull, false);
+                circleFull();
             } else if (tool === 'gomme') {
                 tmpCanvas.addEventListener('mousemove', erase, false);
                 erase();
             }
+
         }, false);
 
         tmpCanvas.addEventListener('mouseup', function () {
             tmpCanvas.removeEventListener('mousemove', brush, false);
             tmpCanvas.removeEventListener('mousemove', trait, false);
             tmpCanvas.removeEventListener('mousemove', rectangle, false);
+            tmpCanvas.removeEventListener('mousemove', rectangleFull, false);
             tmpCanvas.removeEventListener('mousemove', circle, false);
+            tmpCanvas.removeEventListener('mousemove', circleFull, false);
             tmpCanvas.removeEventListener('mousemove', erase, false);
+
+            ctx.globalCompositeOperation = 'source-over';
 
             ctx.drawImage(tmpCanvas, 0, 0);
             tmpCtx.clearRect(0, 0, tmpCanvas.width, tmpCanvas.height);
 
             ppts = [];
         }, false);
+
+        // Sauvegarder le dessin
+        $('.saveCanvas').click(function () {
+           var dataUrl = canvas.toDataURL('image/png');
+           window.open(dataUrl, "toDataURL() image", 'width=canvas.width, height=canvas.height');
+           log(dataUrl);
+        });
     }
 
 });
