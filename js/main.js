190721f965952bcef867d18809ac325d48191365
LilSaint <pamela.carric@gmail.com>
2017-05-14 17:24:26 +0200

    Options tools done

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-/*global console */
-$(function() {
+/*global console, document, $ */
+$(function () {
+    'use strict';
 
     /**
      * Fonction de debug
@@ -11,90 +12,210 @@ $(function() {
     }
     console.log('jQuery Is Ready');
 
+    var canvas          = document.getElementById('myCanvas'),
+        paint           = document.querySelector('.paint'),
+        tmpCanvas       = document.createElement('canvas'),
+        tmpCtx          = tmpCanvas.getContext('2d'),
+        ctx,
+        mouse           = {x: 0, y: 0},
+        last_mouse      = {x: 0, y: 0},
+        start_mouse     = {x: 0, y: 0},
+        ppts            = [];
+
+    var tool = 'brush';
+    $('#tools button').click(function () {
+        tool = $(this).attr('id');
+        log(tool);
+    });
+
+    tmpCanvas.className = 'tmpCanvas';
+    tmpCanvas.width = canvas.width;
+    tmpCanvas.height = canvas.height;
+
+    paint.appendChild(tmpCanvas);
+
+    log(canvas);
+
     /**
-     * Gère my_paint
+     * Dessine le canvas sans options
+     * 
      */
     function drawCanvas() {
-        var canvas = document.getElementById('myCanvas'),
-            mouse = {x: 0, y: 0};
-
-        log(canvas);
-
-        // Vérifie si le canvas est dispo
-        if (canvas.getContext) {
-            var ctx = canvas.getContext('2d');
-
-            ctx.beginPath(); // Initialise un nouveau trajet
-            ctx.moveTo(100, 50); // Position initiale du tracet
-            ctx.lineTo(200, 100); // Déplacement du tracé
-            ctx.fill(); // Trace le dessin
-
-            ctx.lineJoin = 'round';
-            ctx.lineCap = 'round';
-            ctx.strokeStyle = '#000000';
-
-            canvas.addEventListener('mousemove', function (e) {
-               mouse.x = e.pageX - this.offsetLeft;
-               mouse.y = e.pageY - this.offsetTop;
-            }, false);
-
-
-            // Change l'epaisseur du trait
-            $('.largeur').on('input', function() {
-                var largeur = $(".largeur").val();
-                // log(largeur);
-                // log(size);
-                ctx.lineWidth = largeur;
-            });
-
-            // Change la couleur du trait
-            $('.colors').on('change', function () {
-                log('je suis la');
-                var color = $('.colors').val();
-                log(color);
-                ctx.strokeStyle = color;
-            });
-
-            // Gomme
-            $('.gomme').click(function () {
-                eraser();
-            });
-
-            canvas.addEventListener('mousedown', function (e) {
-               ctx.beginPath();
-               ctx.moveTo(mouse.x, mouse.y);
-
-               canvas.addEventListener('mousemove', onPaint, false);
-            }, false);
-
-            canvas.addEventListener('mouseup', function () {
-                canvas.removeEventListener('mousemove', onPaint, false);
-            }, false);
-
-            /**
-             * Pinceau libre
-             */
-            function onPaint() {
-                ctx.lineTo(mouse.x, mouse.y);
-                ctx.stroke();
-            }
-
-            /**
-             * Gomme le canvas de la même façon que le pinceau
-             *
-             */
-            function eraser() {
-                log('Je gomme');
-                canvas.addEventListener('click', function (e) {
-                    mouse.x = e.pageX - this.offsetLeft;
-                    mouse.y = e.pageY - this.offsetTop;
-                    ctx.clearRect(mouse.x, mouse.y, canvas.width, canvas.height);
-                });
-                ctx.globalCompositeOperation = 'destination-out';
-                ctx.strokeStyle = 'rgba(0, 0, 0, 1.0)';
-            }
-        }
+        tmpCtx.beginPath();
+        tmpCtx.moveTo(last_mouse.x, last_mouse.y);
+        tmpCtx.lineTo(mouse.x, mouse.y);
+        tmpCtx.stroke();
+        tmpCtx.closePath();
     }
 
-    drawCanvas();
+    /**
+     * Simule un dessin au pinceau
+     *
+     */
+    function brush() {
+
+        var crayonTexture = new Image();
+        crayonTexture.src = 'img/crayon-texture.png';
+        // crayonTexture.src = 'img/paint-before2.png';
+
+        tmpCtx.drawImage(crayonTexture, 0, 0, tmpCanvas.width, tmpCanvas.height);
+        tmpCtx.beginPath();
+        tmpCtx.moveTo(last_mouse.x, last_mouse.y);
+        tmpCtx.lineTo(mouse.x, mouse.y);
+        tmpCtx.globalAlpha = 0.4;
+    }
+
+    /**
+     * Dessine un trait
+     *
+     */
+    function trait() {
+        tmpCtx.clearRect(0, 0, tmpCanvas.width, tmpCanvas.height);
+
+        tmpCtx.beginPath();
+        tmpCtx.moveTo(start_mouse.x, start_mouse.y);
+        tmpCtx.lineTo(mouse.x, mouse.y);
+        tmpCtx.globalAlpha = 1;
+        tmpCtx.stroke();
+        tmpCtx.closePath();
+    }
+
+    /**
+     * Dessine un rectangle
+     *
+     */
+    function rectangle() {
+        var x       = Math.min(mouse.x, start_mouse.x),
+            y       = Math.min(mouse.y, start_mouse.y),
+            width   = Math.abs(mouse.x - start_mouse.x),
+            height  = Math.abs(mouse.y - start_mouse.y);
+
+        tmpCtx.globalAlpha = 1;
+        tmpCtx.clearRect(0, 0, tmpCanvas.width, tmpCanvas.height);
+        tmpCtx.strokeRect(x, y, width, height);
+    }
+
+    /**
+     * Dessine un cercle
+     *
+     */
+    function circle() {
+        var x,
+            y,
+            radius;
+
+        x = (mouse.x + start_mouse.x) / 2;
+        y = (mouse.y + start_mouse.y) / 2;
+        radius = Math.max(
+            Math.abs(mouse.x - start_mouse.x),
+            Math.abs(mouse.y - start_mouse.y)
+        ) / 2;
+
+        tmpCtx.globalAlpha = 1;
+        tmpCtx.clearRect(0, 0, tmpCanvas.width, tmpCanvas.height);
+        tmpCtx.beginPath();
+        tmpCtx.arc(x, y, radius, 0, Math.PI * 2, false);
+        tmpCtx.stroke();
+        tmpCtx.closePath();
+    }
+
+    /**
+     * Gomme
+     *
+     */
+    function erase() {
+
+        ctx.globalCompositeOperation = 'destination-out';
+        ctx.fillStyle = '#FFF';
+        ctx.strokeStyle = '#FFF';
+        ctx.stroke();
+        ctx.closePath();
+    }
+
+    /* ***************************************************************** */
+    /* ************************ CANVAS BY DEFAULT ********************* */
+    /* ***************************************************************** */
+    if (canvas.getContext) {
+        ctx = canvas.getContext('2d');
+
+        tmpCtx.lineJoin = 'round';
+        tmpCtx.lineCap = 'round';
+
+        // Change l'epaisseur du trait
+        $('.largeur').on('input', function () {
+            var largeur = $(".largeur").val();
+            tmpCtx.lineWidth = largeur;
+        });
+
+        // Change la couleur du trait
+        $('.colors').on('change', function () {
+            var color = $('.colors').val();
+            tmpCtx.strokeStyle = color;
+        });
+
+        tmpCanvas.addEventListener('mousemove', function (evt) {
+            last_mouse.x = mouse.x;
+            last_mouse.y = mouse.y;
+
+            mouse.x = evt.pageX - this.offsetLeft;
+            mouse.y = evt.pageY - this.offsetTop;
+        }, false);
+
+        tmpCanvas.addEventListener('mousedown', function () {
+            tmpCanvas.addEventListener('mousemove', drawCanvas, false);
+        }, false);
+
+        tmpCanvas.addEventListener('mouseup', function () {
+            tmpCanvas.removeEventListener('mousemove', drawCanvas, false);
+        }, false);
+
+        /* ***************************************************************** */
+        /* ************************ MANAGE OPTIONS ************************ */
+        /* ***************************************************************** */
+
+        tmpCanvas.addEventListener('mousemove', function(evt) {
+
+            mouse.x = typeof evt.offsetX !== 'undefined' ? evt.offsetX : evt.layerX;
+            mouse.y = typeof evt.offsetY !== 'undefined' ? evt.offsetY : evt.layerY;
+        }, false);
+        
+        tmpCanvas.addEventListener('mousedown', function (evt) {
+            mouse.x = typeof evt.offsetX !== 'undefined' ? evt.offsetX : evt.layerX;
+            mouse.y = typeof evt.offsetY !== 'undefined' ? evt.offsetY : evt.layerY;
+
+            start_mouse.x = mouse.x;
+            start_mouse.y = mouse.y;
+
+            if (tool === 'brush') {
+                tmpCanvas.addEventListener('mousemove', brush, false);
+                brush();
+            } else if (tool === 'trait') {
+                tmpCanvas.addEventListener('mousemove', trait, false);
+                trait();
+            } else if (tool === 'rectangle') {
+                tmpCanvas.addEventListener('mousemove', rectangle, false);
+                rectangle();
+            } else if (tool === 'circle') {
+                tmpCanvas.addEventListener('mousemove', circle, false);
+                circle();
+            } else if (tool === 'gomme') {
+                tmpCanvas.addEventListener('mousemove', erase, false);
+                erase();
+            }
+        }, false);
+
+        tmpCanvas.addEventListener('mouseup', function () {
+            tmpCanvas.removeEventListener('mousemove', brush, false);
+            tmpCanvas.removeEventListener('mousemove', trait, false);
+            tmpCanvas.removeEventListener('mousemove', rectangle, false);
+            tmpCanvas.removeEventListener('mousemove', circle, false);
+            tmpCanvas.removeEventListener('mousemove', erase, false);
+
+            ctx.drawImage(tmpCanvas, 0, 0);
+            tmpCtx.clearRect(0, 0, tmpCanvas.width, tmpCanvas.height);
+
+            ppts = [];
+        }, false);
+    }
+
 });
